7922a7aa5c00be7588aab94d751466d4b20ad37e
Bart-Desktop <Bartjstolarek@gmail.com>
2024-10-09 23:43:43 +1100

    updated mean revision strategy

--- strategies/MeaniePantsVwap/__init__.py
+++ strategies/MeaniePantsVwap/__init__.py
@@ -8,9 +8,15 @@ from datetime import datetime, timezone, timedelta
 class MeaniePantsVwap(Strategy):
     def __init__(self):
         super().__init__()
-        self.vwap_band_level = 2  # Use the 3rd band for entry signals (index 2 in 0-based list)
         self.candle_index = 0
         self.last_day_timestamp = 0
+        self.band_position = {
+            'first': False,
+            'second': False,
+            'third': False,
+            'fourth': False,
+            'fifth': False
+        }
 
     @property
     @cached
@@ -19,98 +25,155 @@ class MeaniePantsVwap(Strategy):
 
     @property
     @cached
+    def atr_daily(self):
+        daily_candles = self.get_candles(self.exchange, self.symbol, "1D")
+        atr = ta.atr(daily_candles, period=2, sequential=True)
+        return atr
+
+    @property
+    @cached
+    def supertrend_daily(self):
+        daily_candles = self.get_candles(self.exchange, self.symbol, "1D")
+        trend = ta.supertrend(daily_candles, period=2, factor=3, sequential=False).trend
+        if trend < self.price:
+            return 1
+        elif trend > self.price:
+            return -1
+        else:
+            return 0
+
+    @property
+    @cached
     def vwap_bands(self):
         return cta.vwapbands(self.candles, sequential=True)
 
-    def is_within_first_30_minutes(self):
-        # Assuming 1-minute candles, check if we're within the first 30 candles of the day
-        return self.candle_index < 30
+    def is_within_first_x_minutes(self):
+        return self.candle_index < 60
+
+    def is_within_final_hour(self):
+        current_timestamp = self.candles[-1, 0]
+        current_datetime_utc = self.convert_timestamp_to_utc(current_timestamp)
+        
+        # Check if it's the last hour of the day
+        if current_datetime_utc.hour == 23:
+            return True
+        else:
+            return False
 
     def should_long(self) -> bool:
-        return self.price < self.vwap_bands.lower_bands[self.vwap_band_level][-1] and not self.is_within_first_30_minutes()
+        if self.is_within_first_x_minutes() or self.is_within_final_hour() or self.position.is_open:
+            return False
+        
+        if not self.band_position['second'] and self.price <= self.vwap_bands.lower_bands[1][-1]:
+            self.band_position['second'] = True
+            print(f"Band 2 at price {self.price} triggered for short, candle_index: {self.candle_index}")
+            return True
+
+        return False
 
     def should_short(self) -> bool:
-        return self.price > self.vwap_bands.upper_bands[self.vwap_band_level][-1] and not self.is_within_first_30_minutes()
+        if self.is_within_first_x_minutes() or self.is_within_final_hour() or self.position.is_open:
+            return False
+        
+        if not self.band_position['second'] and self.price >= self.vwap_bands.upper_bands[1][-1]:
+            self.band_position['second'] = True
+            print(f"Band 2 at price {self.price} triggered for short, candle_index: {self.candle_index}")
+            return True
+
+        return False
 
     def should_cancel_entry(self) -> bool:
         return False
 
     def go_long(self):
         entry_price = self.price
-        stop_loss = self.price - self.atr[-1] * 2.5
-        take_profit = self.vwap_bands.vwap[-1]
+        stop_loss = self.vwap_bands.lower_bands[4][-1]
 
-        qty = utils.risk_to_qty(capital=self.available_margin, risk_per_capital=3, entry_price=entry_price, stop_loss_price=stop_loss, fee_rate=self.fee_rate)
+        qty = utils.risk_to_qty(capital=self.available_margin, risk_per_capital=1, entry_price=entry_price, stop_loss_price=stop_loss, fee_rate=self.fee_rate)
 
         self.buy = qty, entry_price
         self.stop_loss = qty, stop_loss
-        self.take_profit = qty, take_profit
+        
 
     def go_short(self):
         entry_price = self.price
-        stop_loss = self.price + self.atr[-1] * 2.5
-        take_profit = self.vwap_bands.vwap[-1]
+        stop_loss = self.vwap_bands.upper_bands[4][-1]
 
-        qty = utils.risk_to_qty(capital=self.available_margin, risk_per_capital=3, entry_price=entry_price, stop_loss_price=stop_loss, fee_rate=self.fee_rate)
+        qty = utils.risk_to_qty(capital=self.available_margin, risk_per_capital=1, entry_price=entry_price, stop_loss_price=stop_loss, fee_rate=self.fee_rate)
 
         self.sell = qty, entry_price
         self.stop_loss = qty, stop_loss
-        self.take_profit = qty, take_profit
+
+    def increase_position_size(self, percentage):
+        current_qty = self.position.qty
+        additional_qty = abs(current_qty) * percentage
+        
+        if self.is_long:
+            self.buy = additional_qty, self.price
+        elif self.is_short:
+            self.sell = additional_qty, self.price
 
     def update_position(self):
-        
-        # Get current timestamp
-        current_timestamp = self.candles[-1, 0]
-        current_datetime_utc = self.convert_timestamp_to_utc(current_timestamp)
-        
-        # Check if its last hour of the day
-        if current_datetime_utc.hour == 23:
+        if self.is_short:
+            if self.price >= self.vwap_bands.upper_bands[2][-1] and not self.band_position['third']:
+                self.band_position['third'] = True
+                self.increase_position_size(0.5)  # Increase by 50%
+            elif self.price >= self.vwap_bands.upper_bands[3][-1] and not self.band_position['fourth']:
+                self.band_position['fourth'] = True
+                self.increase_position_size(0.5)  # Increase by 50%
+
+        if self.is_long:
+            if self.price <= self.vwap_bands.lower_bands[2][-1] and not self.band_position['third']:
+                self.band_position['third'] = True
+                self.increase_position_size(0.5)  # Increase by 50%
+            elif self.price <= self.vwap_bands.lower_bands[3][-1] and not self.band_position['fourth']:
+                self.band_position['fourth'] = True
+                self.increase_position_size(0.5)  # Increase by 50%
+
+        if (self.is_long and self.price >= self.vwap_bands.vwap[-1]) or \
+            (self.is_short and self.price <= self.vwap_bands.vwap[-1]):
             self.liquidate()
-        if self.is_long and (self.price >= self.vwap_bands.vwap[-1] or current_datetime_utc.hour == 23):
-            self.liquidate()
-        elif self.is_short and (self.price <= self.vwap_bands.vwap[-1] or current_datetime_utc.hour == 23):
-            self.liquidate()
+            self.reset_band_positions()
+
+    def reset_band_positions(self):
+        self.band_position = {
+            'first': False,
+            'second': False,
+            'third': False,
+            'fourth': False,
+            'fifth': False
+        }
 
     def should_cancel(self) -> bool:
         return False
 
     def convert_timestamp_to_utc(self, timestamp_ms):
-        # Convert milliseconds to seconds
         timestamp_s = timestamp_ms / 1000.0
-        
-        # Create a datetime object in UTC
         return datetime.fromtimestamp(timestamp_s, tz=timezone.utc)
-        
 
-    def after(self) -> None:
-
+    def before(self):
         current_timestamp = self.candles[-1, 0]
         current_datetime_utc = self.convert_timestamp_to_utc(current_timestamp)
-        previous_timestamp = self.candles[-2, 0]
-        previous_datetime_utc = self.convert_timestamp_to_utc(previous_timestamp)
-
-
-        # Check if it's a new day
-        if (current_datetime_utc.date() - previous_datetime_utc.date()) == timedelta(days=1):
+        
+        if self.last_day_timestamp == 0 or current_datetime_utc.date() > self.convert_timestamp_to_utc(self.last_day_timestamp).date():
             self.candle_index = 0
+            self.last_day_timestamp = current_timestamp
+            self.reset_band_positions()
+            print(f"New day started: Timestamp: {current_datetime_utc}, Candle index: {self.candle_index}")
         else:
-            self.candle_index += 1
+            self.candle_index += 1  
 
-        # Add VWAP line
+    def after(self):
         self.add_line_to_candle_chart('VWAP', self.vwap_bands.vwap[-1], color='white')
 
-        # Define colors for bands
         band_colors = ['rgba(255,0,0,0.1)', 'rgba(255,0,0,0.2)', 'rgba(255,0,0,0.3)', 'rgba(255,0,0,0.4)', 'rgba(255,0,0,0.5)']
 
-        # Add bands
         for i, color in enumerate(band_colors):
             upper = self.vwap_bands.upper_bands[i][-1]
             lower = self.vwap_bands.lower_bands[i][-1]
             
-            # Add upper and lower band lines
             self.add_line_to_candle_chart(f'Upper Band {i+1}', upper, color='red')
             self.add_line_to_candle_chart(f'Lower Band {i+1}', lower, color='green')
-            
 
     def terminate(self):
         pass
